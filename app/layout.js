39ecd8b1542d0a8a92c2f652e28442f0830574f8
Mark Machukhu <poptramark@gmail.com>
2025-11-28 11:43:16 +0000

    forget

--- app/layout.js
+++ app/layout.js
@@ -34,7 +34,7 @@ export default function RootLayout({ children }) {
         <Footer />
 
         {/* ✅ Bootstrap JS (for navbar collapse, modals, etc.) */}
- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossOrigin="anonymous"></script>
       </body>
     </html>
   );
