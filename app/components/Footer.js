75d3e532a8c9c3bf78f9dc099760624c7a99c3f5
poptramark-eng <poptramark@gmail.com>
2025-12-06 18:33:31 +0300

    Remove personal info from Footer component
    Removed personal information from the footer component.

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -2,21 +2,14 @@ export default function Footer() {
   return (
     <footer className="bg-dark text-light py-4 mt-auto shadow-sm">
       <div className="container text-center">
-        {/* Personal Info */}
-        <h6 className="fw-bold">Mark Machukhu</h6>
-        <p className="mb-1">
-          Software Engineering Student @ Kisii University | Web Development & Scalable Apps
-        </p>
-        <p className="small text-muted mb-2">
-          Skilled in PHP, MySQL, React, Next.js, Bootstrap, API Design,Flutter & DevOps
-        </p>
+        
 
         {/* Divider */}
         <hr className="border-secondary my-3" />
 
         {/* Legal / Copyright */}
         <p className="small mb-0">
-          © {new Date().getFullYear()} NewsPortal. Built by Mark Machukhu. All rights reserved.
+          © {new Date().getFullYear()} NewsPortal.  by Mark Machukhu. All rights reserved.
         </p>
       </div>
     </footer>
